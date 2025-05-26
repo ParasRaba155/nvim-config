151a9e43d374bcdd090d7bcd84a1548872d96841
Paras Raba <parasgadhvi12345@gmail.com>
2025-05-26 14:48:05 +0530

    feat: add multigrep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,9 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'find files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'find in commited files' })
 vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>td', builtin.diagnostics, { desc = '[?] [T]elescope [D]iagnostics' })
